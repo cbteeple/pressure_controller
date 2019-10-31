23b854702dcf152b9e6b46974ee5a9df9fdb882c
cbteeple <cbteeple@g.harvard.edu>
2019-10-31 16:45:26 -0400

    fixed sensor calibration bug, and added sensor config files rather than hardcoding

---
+++ pressure_control_firmware/config/sensors/ASDXRRX030PDAA5.h
@@ -0,0 +1,9 @@
+
+int SENSOR_MODEL=4;
+
+float sensor_output_min = 0.5;
+float sensor_output_max = 4.5;
+float sensor_output_offset = 0.5;
+
+float sensor_pressure_min = -30; //PSI
+float sensor_pressure_max = 30; //PSI
