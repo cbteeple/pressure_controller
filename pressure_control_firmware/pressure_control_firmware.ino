403c6021cbb76efab13c627e2c86afa3d58f5b01
Clark Teeple <cbteeple@g.harvard.edu>
2019-11-01 09:35:37 -0400

    re-arranged variables in sensor config files to be more logical

--- pressure_control_firmware/pressure_control_firmware.ino
+++ pressure_control_firmware/pressure_control_firmware.ino
@@ -17,8 +17,8 @@
 //Include the config file from the system you are using
 //#include "config/config_pneumatic_teensy.h"
 //#include "config/config_pneumatic_teensy8.h"
-//#include "config/config_pneumatic_teensy7.h"
-#include "config/config_vacuum.h"
+#include "config/config_pneumatic_teensy7.h"
+//#include "config/config_vacuum.h"
 //#include "config/config_hydraulic.h"
 
 //DON'T FORGET TO CHANGE THE NUMBER OF CHANNELS IN THE TRAJ PART OF ALLSETTINGS.H
