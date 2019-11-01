6fe3ddf2e52bb459ae880644828272aa2269d7ba
Clark Teeple <cbteeple@g.harvard.edu>
2019-11-01 09:17:12 -0400

    added raw analog input sensor type

---
+++ pressure_control_firmware/config/sensors/raw_analog_input.h
@@ -0,0 +1,9 @@
+
+int SENSOR_MODEL=99;
+
+float sensor_output_min = 1;
+float sensor_output_max = 1;
+float sensor_output_offset = 1;
+
+float sensor_pressure_min = 1;
+float sensor_pressure_max = 1;
