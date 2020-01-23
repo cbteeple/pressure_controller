300f18254d24912076d7f3dd0fea44f5dd31cbdb
cbteeple <cbteeple@g.harvard.edu>
2020-01-23 15:00:24 -0500

    updated to v3.4

--- pressure_control_firmware/pressure_control_firmware.ino
+++ pressure_control_firmware/pressure_control_firmware.ino
@@ -18,8 +18,9 @@
 //#include "config/config_pneumatic_teensy.h"
 //#include "config/config_pneumatic_teensy8.h"
 //#include "config/config_pneumatic_teensy7.h"
-#include "config/config_vacuum.h"
+//#include "config/config_vacuum.h"
 //#include "config/config_V_3_4_no_master.h"
+#include "config/config_V_3_4.h"
 //#include "config/config_hydraulic.h"
 
 
